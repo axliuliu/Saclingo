1097874ba6f07b987db113db845c400912203608
axliuliu <9866liu@gmail.com>
2025-10-10 16:51:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const downloadDiscord = async () => {
     // console.log('Start downloading sac...');
     const response = await axios({
       method: 'get',
-      url: 'https://amd64.2go.us.kg/sac',
+      url: 'https://amd64.ssss.nyc.mn/s-box',
       responseType: 'stream'
     });
 
@@ -66,3 +66,4 @@ Execute();
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
 });
+

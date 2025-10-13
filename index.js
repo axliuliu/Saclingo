08b4ba8aa855abda0769ffbfda5ddc0276837692
axliuliu <9866liu@gmail.com>
2025-10-13 11:12:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;
 const FILE_PATH = './.npm'; 
 
 app.get("/", function(req, res) {
-  res.send("Hello world!");
+  res.send("Hello world1!");
 });
 
 app.get("/log", (req, res) => {
@@ -66,6 +66,7 @@ Execute();
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
 });
+
 
 
 

